fcaa09711067723b61c97db7879e8acacb98016a
Mukhammadkodir Abdusalomov <99057791+Mukhammadkodir27@users.noreply.github.com>
2023-07-16 00:49:36 +0200

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,36 @@
+# ------------------- Variables -----------------
+
+# String
+my_story = "A thousand splendid suns"
+print(my_story)
+
+# Number
+my_grade = 85
+print(my_grade)
+
+# Boolean
+am_i_young = True  # True must always be with uppercase T
+print(am_i_young)
+
+
+# ------------------ Variable Names ------------------
+
+# Rule 1 - Variable names must make sense
+
+# Rule 2 - Python  convention is to use lowercase letters
+my_name = "Mukhammadkodir"  # varibale name is lowercase letters
+
+# Rule 3 - Python is snake case
+# myName -- camel case
+# user_info -- snake case (underscore seperates two different words)
+user_info = "Online"  # snake case
+
+# Rule 4 - Variable names must start with a letter or _
+# 15street - gives an error
+# street15 - is correct
+
+# Rule 5 - Following the pep8 standards
+# to_do = "to buy the products" - correct
+# to_do="to buy the products" - incorrect
+
+movie_rating = 8.5
