edb1d35a8ffcb96635834881b8c23756822bbc7a
Mukhammadkodir Abdusalomov <99057791+Mukhammadkodir27@users.noreply.github.com>
2023-07-16 04:17:25 +0200

    Create none.py

---
+++ none.py
@@ -0,0 +1,13 @@
+# -------------- None ---------------
+
+# The Absence of a value
+
+print(None)
+print(type(None))
+
+# when you don't know the value of a variable but you want to declare
+# as you might need it in future and will know its exact value
+# you can use None:
+
+user_password = None
+print(user_password)
