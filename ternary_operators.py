4d4f265c5e8dd6ca0a79b93453a2d05577f784ab
Mukhammadkodir Abdusalomov <99057791+Mukhammadkodir27@users.noreply.github.com>
2023-08-03 00:28:42 +0200

    Create ternary_operators.py

---
+++ ternary_operators.py
@@ -0,0 +1,16 @@
+# ------------- Ternary Operator ---------------
+
+score = 74
+#if score >= 75:
+#    print("Passed")
+#else: 
+#    print("Failed")
+
+if score >= 75:
+    result = "Passed"
+else:
+    result = "Failed"
+
+print(result)         
+ 
+ 
