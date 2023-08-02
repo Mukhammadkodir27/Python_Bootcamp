130c8fb898232abac421648164d17605dbcb0b2a
Mukhammadkodir Abdusalomov <99057791+Mukhammadkodir27@users.noreply.github.com>
2023-08-03 00:27:29 +0200

    Create comparison_operators.py

---
+++ comparison_operators.py
@@ -0,0 +1,18 @@
+# -------------- Comparison Operators --------------
+
+print(12 > 23)  # False
+print(14 >= 4)  # True
+print(14 < 4)   # False
+print(14 == 14)  # True
+print(14 == "14")  # False because of their type
+print(14 != "14")  # True because they are not equal because of their type
+print("cat" > "dog") # False becuase of numeric representation of dog is bigger then num rep of cat
+print("cat" == "CAT") # False
+
+# Built-in method for checking the numerical representations of strings
+print(ord("c"))  # 99
+print(ord("d"))  # 100
+print(ord("a"))  # 97
+print(ord("A"))  # 65
+
+
