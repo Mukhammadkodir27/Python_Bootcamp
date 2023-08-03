604ef6dbddcdce47ea3ea08df96e00c5dde908f7
Mukhammadkodir Abdusalomov <99057791+Mukhammadkodir27@users.noreply.github.com>
2023-08-03 07:48:01 +0200

    Create while_loops.py

---
+++ while_loops.py
@@ -0,0 +1,7 @@
+# ---------- While Loops ----------
+
+temperature = 40
+
+while temperature >= 20:
+    print(f"It is good day to go out, and the temperature is {temperature}")
+    temperature = temperature - 5   # or temperature -= 5
