66db1fffa9a5fe26937f940fe8dbaf0bfc07aeb2
Mukhammadkodir Abdusalomov <99057791+Mukhammadkodir27@users.noreply.github.com>
2023-08-03 07:37:59 +0200

    Update iterables.py

--- iterables.py
+++ iterables.py
@@ -1,1 +1,11 @@
+# ---------- Iterators ----------
+
+print(type(range(5)))
+
+for char in "Mukhammadkodir":
+    print(char)
+
+# Lists are also iterable
+for smth in ["Play with the cat", "walk the dog", "close the door"]:
+    print(smth)
 
