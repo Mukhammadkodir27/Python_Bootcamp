01d480b3f8c971a56c2854763a40a145cc08dcf3
Mukhammadkodir Abdusalomov <99057791+Mukhammadkodir27@users.noreply.github.com>
2023-08-03 07:01:08 +0200

    Update nested_loops.py

--- nested_loops.py
+++ nested_loops.py
@@ -1,1 +1,10 @@
-# --------- Nested Loops --------
+# ---------- Nested Loop ----------
+
+# for x in range(3):
+#     for y in range(6):
+#         print(f"Point({x}, {y})")
+
+for x in range(5):
+    for y in range(2):
+        for z in range(3):
+            print(f"Point({x}, {y}, {z})")
