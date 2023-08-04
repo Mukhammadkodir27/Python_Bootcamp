5d4739c488e83f38f7b97bdce3bd9ba05d73d726
Mukhammadkodir Abdusalomov <99057791+Mukhammadkodir27@users.noreply.github.com>
2023-08-04 09:07:13 +0200

    Update 3_required_optional_params.py

--- 3_required_optional_params.py
+++ 3_required_optional_params.py
@@ -1,1 +1,20 @@
-1
+# ---------- Required & Optional Parameters ----------
+
+# Example
+def working_condition(device, status="working"):
+    return f"The {device} is {status}"
+
+
+print(working_condition("Laptop"))
+# overriding the optional parameter
+print(working_condition("fridge", "out of order"))
+
+
+# Example
+def subtract(x, z, y=15):    # it gives error because optional parameter has to be the last one
+    return x - y - z
+
+
+print(subtract(25, 5))
+print(subtract(25, 5, 10))  # overriding the above optional parameter
+
