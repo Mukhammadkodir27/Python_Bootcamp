77cff4f2f0b9abf22744766b4f49b4fb5031efda
Mukhammadkodir Abdusalomov <99057791+Mukhammadkodir27@users.noreply.github.com>
2023-08-04 05:23:06 +0200

    Update intro_to_functions.py

--- intro_to_functions.py
+++ intro_to_functions.py
@@ -1,1 +1,29 @@
-# Introduction to functions
+# ---------- Introduction to functions ----------
+
+# Example 1
+def book():
+    print("Thriller")
+
+
+book()
+
+# Example 2
+success = True
+
+
+def operation():
+    if success:   # success: same as success == True
+        print("Yaay!")
+
+
+operation()
+
+
+# Example 3
+def sum():
+    for number in range(4, 34, 4):
+        print(number)
+
+
+sum()
+
