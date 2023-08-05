0b3bfc16fc20e2716bab384c57311a2e1083501e
Mukhammadkodir Abdusalomov <99057791+Mukhammadkodir27@users.noreply.github.com>
2023-08-05 05:14:32 +0200

    Update 4_intro_to_lists.py

--- 4_intro_to_lists.py
+++ 4_intro_to_lists.py
@@ -1,1 +1,31 @@
+# ---------- Lists Introduction ----------
+
+# Example ->>> simple lists
+numbers = [1, 2, 3, 12, 64, 124, 654]
+names = ["John", "Tom", "Mark", "Elon"]
+# print(names)
+# print(numbers)
+
+# Example ->>> a list of lists
+likes = [["tv", "pc", "laptop"], [
+    "gaming", "watching", "fun"], ["pizze", "food"]]
+# print(likes)
+
+
+# Example ->>> a list of lists of lists
+mixed = [[1, True], "Cat", "Dog", [["wind", "water"], ["earth", "fire"]]]
+# print(mixed)
+
+# Example ->>> a list of identical items
+animal = ["cat"] * 10
+# print(animal)
+
+# Example ->>> list concatenation/merging
+even_nums = [2, 4, 6, 8, 10]
+odd_nums = [1, 3, 5, 7, 9]
+cities = ["Tashkent", "New York", "London"]
+booleans = [True, False]
+
+mixed_data = even_nums + odd_nums + cities + booleans
+print(mixed_data)
 
