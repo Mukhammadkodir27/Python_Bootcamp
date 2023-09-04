ab1cb16f5d4fb9487bb18617e7106f47ef1c755e
Mukhammadkodir Abdusalomov <99057791+Mukhammadkodir27@users.noreply.github.com>
2023-09-04 06:09:01 +0200

    Update 5_Dictionaries.py

--- 5_Dictionaries.py
+++ 5_Dictionaries.py
@@ -59,3 +59,4 @@ print("Hallo", employee_info.get("Favorite Color"))
 print(employee_info.get("Favorite Color", "Green"))
 # in case if value of this key provided does not exists, then this value given with key will be used
 
+1
