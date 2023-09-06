bdfd9eb63ca63fc482b5c02a0af9865eb02145f1
Mukhammadkodir Abdusalomov <99057791+Mukhammadkodir27@users.noreply.github.com>
2023-09-06 06:33:59 +0200

    Update 5_Dictionaries.py

--- 5_Dictionaries.py
+++ 5_Dictionaries.py
@@ -58,3 +58,43 @@ print("Hallo", employee_info.get("Favorite Color"))
 # Example 7
 print(employee_info.get("Favorite Color", "Green"))
 # in case if value of this key provided does not exists, then this value given with key will be used
+
+
+# ---------- Dictionary Methods Part 1 ----------
+
+players = {
+    "winger": "Kadir",
+    "striker": "Adil",
+    "defender": "Fozil",
+    "winger_number": 7,
+    "subs": "Bin, Maiwand, Gourab",
+    11: "squad amount",
+    "Best Player": "Kadir, 7, Uzbekistan",
+    "Active Players": True,
+    "Statistics": {"Win", "Win", "Lose", "Win"},
+    "Scores": ["11:1", "0:3", "5:4"],
+    "Cups": ("Sawo Gruz", "Nations Cup")
+}
+
+# clear()
+players.clear()  # clears our dictionary
+
+# copy()
+new_squad = players.copy()
+# if you change this new dictionary, original one will be unchanged (it remains same)
+new_squad["subs"] = "Kadir, Adil, Fozil"
+new_squad["Active Players"] = False
+
+
+# fromkeys()  - accept the sequences
+letters = {'a', 'e', 'i', 'o', 'u'}
+numbers = [1, 2]
+
+vowels = dict.fromkeys(letters)  # none automatically
+vowels = dict.fromkeys(letters, numbers)
+# all these items will have a value provided secondly as arg
+
+
+# The {} object
+print({}.fromkeys(players))   # {} means a dictionary
+
