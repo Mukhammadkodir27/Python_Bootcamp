9dda7980ff27dc4eb04d7c47f0bcea74a441fe39
Mukhammadkodir Abdusalomov <99057791+Mukhammadkodir27@users.noreply.github.com>
2023-09-12 07:14:14 +0200

    Update 6_Tuples.py

--- 6_Tuples.py
+++ 6_Tuples.py
@@ -1,1 +1,77 @@
+# ---------- Introduction to Tuples ----------
+
+# It is similar to Lists but ...
+# we cannot change the elements of tuples once they are assigned
+
+first_tuple = ()
+print(first_tuple)
+print(type(first_tuple))
+
+num = (1, 2, 3, 4)
+print(num)
+print(type(num))
+
+# it can have mixed items of mixed data types, in many amounts
+mixed_tuple = (1, "Bear", True)
+print(mixed_tuple)
+
+# mixed data types
+mixed = ({"Name": "John", "Age": 18, "Job": "Developer"}, [
+         "Dev1", "Dev2", 1, 4], (1.2, 1.3, 2, 3), {"Europe", 19})
+print(mixed)
+
+print("")
+print("")
+print("")
+
+# ----- Tuple Packing & Unpacking -----
+
+# Example ---> Tuple Packing
+to_do = "buy coffee", "read a book", "finish homework"  # it is a tuple
+print(to_do)
+
+# Example ---> Tuple Unpacking
+to_do1, to_do2, to_do3 = to_do
+print(to_do1)
+print(to_do2)
+print(to_do3)
+
+# Example ---> Tuple with one element
+name1 = ("Tommy")  # it is a string
+print(name1)
+print(type(name1))
+name = ("Tommy",)  # it is a tuple as there is a comma after "Value"
+print(name)
+print(type(name))
+
+print("")
+print("")
+print("")
+
+# ----- Accessing Tuple Elements -----
+
+# Example ---> Indexing []
+mixed2 = (False, 3.14159, "Python", ["Web", "Mobile"], 45)
+print(mixed2)
+print(mixed2[0])
+print(mixed2[1])
+
+for i in mixed2:
+    print(i)
+
+
+# Example ---> Negative Indexing
+print(mixed2[-1])   # 45 because it starts from right-hand side till left side
+print(mixed2[-2])  # ["Web", "Mobile"]
+print(mixed2[-5])  # False
+
+
+# Example ---> Slicing
+print(mixed2[0:2])
+print(mixed2[2:4])
+print(mixed2[:4])
+print(mixed[3:])
+print(mixed[:])
+print(mixed[:-1])  # from -5, -4, -3, -2 till -1
+print(mixed[:-3])  # from -5, -4 till -3
 
