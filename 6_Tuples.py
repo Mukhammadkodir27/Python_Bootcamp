2bd24aaac7ffc45446664a61dac3b842bff6447a
Mukhammadkodir Abdusalomov <99057791+Mukhammadkodir27@users.noreply.github.com>
2023-09-12 06:25:52 +0200

    Update 6_Tuples.py

--- 6_Tuples.py
+++ 6_Tuples.py
@@ -20,5 +20,26 @@ mixed = ({"Name": "John", "Age": 18, "Job": "Developer"}, [
          "Dev1", "Dev2", 1, 4], (1.2, 1.3, 2, 3), {"Europe", 19})
 print(mixed)
 
+print("")
+print("")
+print("")
 
 # ----- Tuple Packing & Unpacking -----
+
+# Example ---> Tuple Packing
+to_do = "buy coffee", "read a book", "finish homework"  # it is a tuple
+print(to_do)
+
+# Example ---> Tuple Unpacking
+to_do1, to_do2, to_do3 = to_do
+print(to_do1)
+print(to_do2)
+print(to_do3)
+
+# Example ---> Tuple with one element
+name1 = ("Tommy")  # it is a string
+print(name1)
+print(type(name1))
+name = ("Tommy",)  # it is a tuple as there is a comma after "Value"
+print(name)
+print(type(name))
