1e3ac0bf4bf13228fb7409068d0c8f9e1a461692
Mukhammadkodir Abdusalomov <99057791+Mukhammadkodir27@users.noreply.github.com>
2023-09-25 01:31:59 +0200

    Update 8_filter_function.py

--- 8_filter_function.py
+++ 8_filter_function.py
@@ -1,12 +1,1 @@
-# ---------- Filter Function ----------
 
-letters = ['a', 'b', 'd', 'e', 'i', 'j', 'o']
-vowels = "aeio"
-counter = 0
-
-for vowel in vowels:
-    if vowel in letters:
-        counter = counter + 1
-
-
-print("There are", counter, "vowels")
