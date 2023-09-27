068853167f3720286a6519c7ac95e2575d35c896
Mukhammadkodir Abdusalomov <99057791+Mukhammadkodir27@users.noreply.github.com>
2023-09-27 19:05:11 +0200

    Update 8_Anonymous_or_Lambda_Functions.py

--- 8_Anonymous_or_Lambda_Functions.py
+++ 8_Anonymous_or_Lambda_Functions.py
@@ -2,3 +2,19 @@
 
 # Iterators can be iterated only once
 
+products = [
+    ("Item1", 10),
+    ("Item2", 12),
+    ("Item3", 3),
+    ("Item4", 5),
+    ("Item5", 16),
+    ("Item6", 4)
+]
+
+products.sort(key=lambda product: product[1])
+print(products)
+
+print("")
+
+products.sort(key=lambda items: items[0])
+print(products)
