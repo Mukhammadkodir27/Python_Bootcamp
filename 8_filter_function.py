44590bdb9d40eebddc05c9e8a0a78c27be05ad88
Mukhammadkodir Abdusalomov <99057791+Mukhammadkodir27@users.noreply.github.com>
2023-09-27 17:31:56 +0200

    Update 8_filter_function.py

--- 8_filter_function.py
+++ 8_filter_function.py
@@ -1,1 +1,5 @@
-here we go again...
+# ---------- Filter Function ----------
+
+
+# filter function takes 2 parameters, one is function that we create and the second one is an iterable. 
+# or filter function could be as None but this to be continued...
