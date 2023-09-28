6edc70588bce09256487608af57c681d8326a7d7
Mukhammadkodir Abdusalomov <99057791+Mukhammadkodir27@users.noreply.github.com>
2023-09-29 00:09:27 +0200

    Update 8_the_Zip_function.py

--- 8_the_Zip_function.py
+++ 8_the_Zip_function.py
@@ -1,1 +1,3 @@
 # ---------- Zip Function ----------
+
+# result = zip(iterator1, iterator2, iterator3) # the code will run according to the smallest item, if iterator2 has 3 elements, then it will be executed 3 times only
