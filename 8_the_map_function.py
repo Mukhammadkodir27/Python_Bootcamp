0c4f0ff5b3a849bc5b942b50361843ea54231625
Mukhammadkodir Abdusalomov <99057791+Mukhammadkodir27@users.noreply.github.com>
2023-09-28 16:18:59 +0200

    Update 8_the_map_function.py

--- 8_the_map_function.py
+++ 8_the_map_function.py
@@ -1,1 +1,15 @@
 # ---------- The Map Function ----------
+
+# map functions take 2 paramteres, first one is function and the second one is iterable 
+
+# Iterable 
+numbers = [1, 2, 3, 4, 5]
+
+# Function 
+def calculate_square(n):
+  return n * n
+
+results = map(calculate_square, numbers)
+results_in_list = list(results)
+print(results_in_list)
+
