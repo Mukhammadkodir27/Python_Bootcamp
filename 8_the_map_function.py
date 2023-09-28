4af01deeeea8f4263aa3efb60c1d31dcd8028c48
Mukhammadkodir Abdusalomov <99057791+Mukhammadkodir27@users.noreply.github.com>
2023-09-28 17:11:24 +0200

    Update 8_the_map_function.py

--- 8_the_map_function.py
+++ 8_the_map_function.py
@@ -23,3 +23,45 @@ print(tuple(results))
 # nums2 = [1, 2]
 result2 = map(lambda n1, n2: n1 + n2, nums1, nums2) 
 # this time map function will be executed 2 times only, as the second iterable named nums2 consists of 2 elements only.
+
+
+# ----------------------------------------------------------------------------------------------------------------------------
+
+
+# Iterable
+numbers = [1, 2, 3, 4, 5, 6]
+
+# Function
+
+
+def calculate_square(n):
+    return n * n
+
+
+result_of_calculation = map(calculate_square, numbers)
+print(tuple(result_of_calculation))
+# print(result_of_calculation)
+
+# or you can use lambda with map function
+
+letters = ['a', 'b', 'c']
+
+result_of_letters = map(lambda x: x, letters)
+print(list(result_of_letters))
+
+# or map + lambda with numbers
+
+numbers = (2, 4, 6, 8)
+result = map(lambda x: x * x, numbers)
+# print(set(result))
+result_list = list(result)
+print(result_list)
+
+# or map + lambda with multiple iterables
+
+num1 = [1, 2, 3, 4, 5]
+num2 = [6, 7]
+
+result_nums = map(lambda n1, n2: (n1 + n2)/2, num1, num2)
+print(list(result_nums))
+print(set(result_nums))  # empty as it can be used only once
