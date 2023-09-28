e3527fbf2cc1a312e07e111e30969907eac1b0e8
Mukhammadkodir Abdusalomov <99057791+Mukhammadkodir27@users.noreply.github.com>
2023-09-28 16:27:09 +0200

    Update 8_the_map_function.py

--- 8_the_map_function.py
+++ 8_the_map_function.py
@@ -16,3 +16,10 @@ print(results_in_list)
 # or it could be done like this
 print(tuple(results))
 
+
+# when there are more than one iterable passed to map function using lambda keyword, then it will be executed in the amount of time regarding the smallest iterable
+# imagine you have 2 iterables
+# nums1 = [1, 2, 3, 4, 5]
+# nums2 = [1, 2]
+result2 = map(lambda n1, n2: n1 + n2, nums1, nums2) 
+# this time map function will be executed 2 times only, as the second iterable named nums2 consists of 2 elements only
