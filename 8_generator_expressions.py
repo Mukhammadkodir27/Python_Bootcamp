21964a5fe717c4ca13bcfe6e048c8949468ba418
Mukhammadkodir Abdusalomov <99057791+Mukhammadkodir27@users.noreply.github.com>
2023-09-29 16:31:13 +0200

    Update 8_generator_expressions.py

--- 8_generator_expressions.py
+++ 8_generator_expressions.py
@@ -11,3 +11,24 @@ print("GO Size: ", getsizeof(numbers_gen)) # 112
 # when we talk about extremely large data, generator expressions are user-friendly. 
 # if in range(100000) list reaches 812133 bytes of memory
 # in (100000000) range also generator expressions remain same as 112. So less memory is used.
+
+
+# List Comprehension
+numbers_list = [x * 2 for x in range(10)]
+numbers_list = [x * 2 for x in range(100)]
+numbers_list = [x * 2 for x in range(1000)]
+numbers_list = [x * 2 for x in range(10000)]
+
+# Generator Expression Objects
+# or
+# Generator Objects
+numbers_gen = (x * 2 for x in range(10))
+numbers_gen = (x * 2 for x in range(100))
+numbers_gen = (x * 2 for x in range(1000))
+numbers_gen = (x * 2 for x in range(10000))
+
+print(type(numbers_gen))
+print(numbers_gen)
+
+print("List Comprehension Expression size:", getsizeof(numbers_list)) # 85175 bytes
+print("Generator Object size:", getsizeof(numbers_gen)) # 208 bytes
