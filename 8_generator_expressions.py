a5fc54e8340cee0bda90b556f60318b14d0af66a
Mukhammadkodir Abdusalomov <99057791+Mukhammadkodir27@users.noreply.github.com>
2023-09-29 15:52:21 +0200

    Update 8_generator_expressions.py

--- 8_generator_expressions.py
+++ 8_generator_expressions.py
@@ -1,1 +1,13 @@
 # ---------- Generator Expressions ----------
+from sys import getsizeof
+# when we are talking about big amount of numbers then it is good to use generator expressions than other data types like list
+# List 
+numbers_list = [x * 2 for x in range(10))
+print("List Size: ", getsizeof(numbers_list)) # 800
+# Generator Expressions
+numbers_gen = (x * 2 for x in range(10)) 
+print("GO Size: ", getsizeof(numbers_gen)) # 112
+
+# when we talk about extremely large data, generator expressions are user-friendly. 
+# if in range(100000) list reaches 812133 bytes of memory
+# in (100000000) range also generator expressions remain same as 112. So less memory is used.
