b65c5d4500ea33feeb44b15b765360a7eef6bc8c
Mukhammadkodir Abdusalomov <99057791+Mukhammadkodir27@users.noreply.github.com>
2023-10-09 02:38:36 +0200

    Update 9_Exception_Handling.py

--- 9_Exception_Handling.py
+++ 9_Exception_Handling.py
@@ -1,1 +1,40 @@
-# --------- Exception Handling ---------
+# ---------- Exception Handling ----------
+
+import sys
+
+# try except
+
+try:
+    pass
+except:
+    pass
+
+
+# Example
+
+try:
+    age = int(input("Enter your age: "))
+except:
+    print("Please, provide a valid value!")
+
+
+# Example
+
+try:
+    age2 = int(input("Enter your age 2: "))
+except ValueError:
+    print("Please, provide a valid value!")
+else:
+    print("No Exceptions here")
+
+
+# Example
+
+try:
+    age3 = int(input("Enter your age 3:"))
+except ValueError as exp_error:
+    print("Please, provide a valid value!")
+    print(exp_error)
+    print(type(exp_error))
+else:
+    print("No Exceptions here!")
