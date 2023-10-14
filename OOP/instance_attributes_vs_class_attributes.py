c6e31530a274b3046362dfe119511a9611d2a133
Mukhammadkodir Abdusalomov <99057791+Mukhammadkodir27@users.noreply.github.com>
2023-10-14 10:10:17 +0200

    Update instance_attributes_vs_class_attributes.py

--- OOP/instance_attributes_vs_class_attributes.py
+++ OOP/instance_attributes_vs_class_attributes.py
@@ -1,1 +1,35 @@
+# ---------- instance attributes vs class attributes ---------
+
+# Objects in Python are dynamic, you can provide attributes to the objects after
+# you have defined the constructor of the class.
+
+class Robot:
+    # class attribute
+    z = 23
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def walk(self):
+        print(f"walking coordinates - ({self.x}, {self.y}, {self.z})")
+
+
+robot1 = Robot(12, 12)
+robot1.walk()
+
+print(robot1.x)
+print(robot1.z)
+
+robot2 = Robot(21, 21)
+robot2.m = 22
+print(robot2.m)
+# print(robot1.m) # attributeError because m is assigned to robot2 only and
+# cannot be accessed by other instances of the class
+
+
+"""
+1) Instance(Object) Attributes - are defined within the constructor of a class
+2) Class Attributes - are created within the class itself
+"""
 
