1951d77ba25a5218adab5140a1c6d8758746c38d
Mukhammadkodir Abdusalomov <99057791+Mukhammadkodir27@users.noreply.github.com>
2023-10-14 04:43:20 +0200

    Update creating_classes.py

--- OOP/creating_classes.py
+++ OOP/creating_classes.py
@@ -1,1 +1,20 @@
+# ---------Creating Classes Objects ---------
 
+class Robot:
+    def walk(self):
+        print("The robot is walking!")
+
+
+robot = Robot()
+robot.walk()
+
+print(type(robot))
+
+# Example 2 ->>>> isinstance() method to check if it is object of the class
+
+# two parameters should be given: 1) object 2) class  --- isinstance(object, class)
+
+print(isinstance(robot, Robot))
+
+robot_obj = Robot()
+print(isinstance(robot_obj, Robot))
