b1a50cf163ab94bba71826d4f6f400f768f4554b
Mukhammadkodir Abdusalomov <99057791+Mukhammadkodir27@users.noreply.github.com>
2024-01-31 18:55:30 +0100

    Update towardsmomentum.py

--- towardsmomentum.py
+++ towardsmomentum.py
@@ -3,4 +3,4 @@ Project Ideas to be completed:
 
 2) AI responsive Chat Talk
 you will talk to the app and it will talk to you back, similar to Bixby, you will ask questions and it will responde. so it can be your best friend when loneliness
-meaning that you can talk to ai when you feel alone.
+meaning that you can talk to ai when you feel you are alone.
