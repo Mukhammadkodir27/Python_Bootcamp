9700b094cf01592c283d3c3115f89f6930fd9b6b
Mukhammadkodir Abdusalomov <99057791+Mukhammadkodir27@users.noreply.github.com>
2024-01-31 18:55:08 +0100

    Update towardsmomentum.py

--- towardsmomentum.py
+++ towardsmomentum.py
@@ -1,4 +1,6 @@
 Project Ideas to be completed: 
 1) Weather Mobile App - using Python
 
-2) 
+2) AI responsive Chat Talk
+you will talk to the app and it will talk to you back, similar to Bixby, you will ask questions and it will responde. so it can be your best friend when loneliness
+meaning that you can talk to ai if you seem alone.
