0d86b1e9ec76790aee571d830b0d5eee74141761
Mukhammadkodir Abdusalomov <99057791+Mukhammadkodir27@users.noreply.github.com>
2024-02-01 20:05:46 +0100

    Update towardsmomentum.py

--- towardsmomentum.py
+++ towardsmomentum.py
@@ -4,3 +4,5 @@ Project Ideas to be completed:
 2) AI responsive Chat Talk
 you will talk to the app and it will talk to you back, similar to Bixby, you will ask questions and it will responde. so it can be your best friend when loneliness
 meaning that you can talk to ai when you feel you are alone.
+
+3) Web App - Mobile App for downlaoding music and listening to music like Spotify
