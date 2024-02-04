c622b10e7fb89f515a4976ce64cab0217469e5d3
Mukhammadkodir Abdusalomov <99057791+Mukhammadkodir27@users.noreply.github.com>
2024-02-04 06:00:28 +0100

    Create 7.py

---
+++ feat/7.py
@@ -0,0 +1,32 @@
+# Finding List Items Index
+
+# numbers = ["m", "nnn", "", 1, 1] * 5
+# print(numbers)
+
+ft_group = ['Kadir', 'Adil', 'Fozil', 'Hasan', 'Hasan', 'Kadir']  # * 2
+
+print(ft_group.index('Fozil'))
+selected_user_index = ft_group.index("Kadir")
+print(selected_user_index)
+
+
+def change_selection(user):
+    global selected_user_index
+    if user in ft_group:
+        selected_user_index = ft_group.index(user)
+
+
+for i in ft_group:
+    if ft_group.count(i) < 2:
+        change_selection(i)
+        print(
+            f"The selected users index is: {selected_user_index} and he is {ft_group[selected_user_index]}")
+        print(f"{ft_group[selected_user_index]} appeared",
+              ft_group.count(i), "time")
+    else:
+        print(
+            f"The selected users index is: {selected_user_index} and he is {ft_group[selected_user_index]}")
+        print(f"{ft_group[selected_user_index]} appeared",
+              ft_group.count(i), "times")
+        continue
+        # break could be used but not recommended!
