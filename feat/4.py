1bda5b30d30bc895d057c8af3e06836cc2ba0baf
Mukhammadkodir Abdusalomov <99057791+Mukhammadkodir27@users.noreply.github.com>
2024-02-04 05:59:40 +0100

    Create 4.py

---
+++ feat/4.py
@@ -0,0 +1,17 @@
+animal = ["cat"] * 10
+
+print(animal)
+
+for an in animal:
+    print(an)
+
+# such variables like strings are called as iterables, as they can be iterated from their own elements
+name = "Mukhammadkodir"
+# lists are also iterables
+
+print("--*--*--*--")
+
+for n in name:
+    print(n)
+
+print(name + " " + "Abdusalomov")
