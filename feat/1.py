079deacbdf5e016800b8ed74dc7c9d856f9b396e
Mukhammadkodir Abdusalomov <99057791+Mukhammadkodir27@users.noreply.github.com>
2024-02-04 02:07:14 +0100

    Create 1.py

---
+++ feat/1.py
@@ -0,0 +1,17 @@
+for n in range(10):
+    print(f"Seconds {n+1}:")
+
+
+name = input("Enter your name: ")
+print(f"Hi {name}")
+
+
+def nested_for_loops():
+    for a in range(5):
+        for b in range(3):
+            for c in range(2):
+                print(f"Point({a}, {b}, {c})")
+
+
+if name:
+    nested_for_loops()
