122fea6889892b58d9e6ac76059be847d9040bae
Mukhammadkodir Abdusalomov <99057791+Mukhammadkodir27@users.noreply.github.com>
2024-04-30 15:05:31 +0200

    Update towardsmomentum.py

--- towardsmomentum.py
+++ towardsmomentum.py
@@ -6,3 +6,5 @@ you will talk to the app and it will talk to you back, similar to Bixby, you wil
 meaning that you can talk to ai when you feel you are alone.
 
 3) Web App - Mobile App for downlaoding music and listening to music like Spotify
+
+4) Essay Checker
