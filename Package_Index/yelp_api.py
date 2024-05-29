91b9e56268231d54a1a28ba7adb04c4d2bda4433
Mukhammadkodir Abdusalomov <99057791+Mukhammadkodir27@users.noreply.github.com>
2024-05-29 22:56:32 +0200

    Create yelp_api.py

---
+++ Package_Index/yelp_api.py
@@ -0,0 +1,20 @@
+# Yelp API
+
+#! Standard HTTP requests
+# GET   ->>> getting the data
+# POST  ->>> creating the data
+# PUT   ->>> updating the data
+# DELETE   ->>> deleting the data
+
+# https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods
+
+"""
+! HTTP response status codes
+Informational responses    ->>> (100-199)
+Successful responses       ->>> (200-299)
+Redirects                  ->>> (300-399)
+Client errors              ->>> (400-499)
+Server errors              ->>> (500-599)
+
+https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+"""
