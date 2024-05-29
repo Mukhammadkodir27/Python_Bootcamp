33d10615f1e62e53550b2a102dd5010210ef324f
Mukhammadkodir Abdusalomov <99057791+Mukhammadkodir27@users.noreply.github.com>
2024-05-29 06:32:16 +0200

    Create venv.py

---
+++ Package_Index/venv.py
@@ -0,0 +1,9 @@
+# Virtual Environment
+
+# pip install pipenv
+# pipenv --version
+# pipenv install requests   #or (module name)
+# pipenv shell
+# exit or # deactivate
+
+# it will create Pipfile and Pipfile.lock
