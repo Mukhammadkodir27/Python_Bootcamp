c9f4fd0d19c207992378df27ff16a543740e333f
Mukhammadkodir Abdusalomov <99057791+Mukhammadkodir27@users.noreply.github.com>
2024-10-26 00:29:39 +0200

    testing_list_method_in_random_ways

---
+++ feat/garbage_code.py
@@ -0,0 +1,7 @@
+name = list("Elbek")    # [E,  l,  b,  e,  k]
+for i in name:
+    print(i)
+print(name)             # [E, l, b, e, k]
+name = ["Elbek"]        # ["Elbek"]
+for i in name:
+    print(i)            # Elbek
