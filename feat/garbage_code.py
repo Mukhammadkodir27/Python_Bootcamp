bab68517b9f6be762589b3f8df826b1753cfdf37
Mukhammadkodir Abdusalomov <99057791+Mukhammadkodir27@users.noreply.github.com>
2024-10-26 18:26:57 +0200

    Update garbage_code.py

--- feat/garbage_code.py
+++ feat/garbage_code.py
@@ -5,3 +5,23 @@ print(name)             # [E, l, b, e, k]
 name = ["Elbek"]        # ["Elbek"]
 for i in name:
     print(i)            # Elbek
+
+numbers = [1, 2, 3, 4, 5]
+fruits = ["Apple", "Banana", "Mango"]
+
+multiplication = [i*i for i in numbers]
+print(multiplication)
+
+nums = list(range(1, 15))
+nums_multiplied = [n*n for n in nums]
+print(nums_multiplied)
+
+nums = list(range(1, 15))
+nums_multiplied = []
+for n in nums:
+    nums_multiplied.append(n//2)
+
+print(nums_multiplied)
+
+fruits2 = [fruit.lower() for fruit in fruits]
+fruits2 = [fruit.upper() for fruit in fruits]
