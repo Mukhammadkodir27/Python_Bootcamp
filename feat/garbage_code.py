a1de9430fc094b2f07054df6adf3ac14f022b84e
Mukhammadkodir Abdusalomov <99057791+Mukhammadkodir27@users.noreply.github.com>
2024-11-08 17:08:27 +0100

    Update garbage_code.py

--- feat/garbage_code.py
+++ feat/garbage_code.py
@@ -25,3 +25,73 @@ print(nums_multiplied)
 
 fruits2 = [fruit.lower() for fruit in fruits]
 fruits2 = [fruit.upper() for fruit in fruits]
+
+
+# # # fromkeys()  - accept the sequences
+# # letters = {'a', 'e', 'i', 'o', 'u'}
+# # numbers = [1, 2]
+
+# # vowels = dict.fromkeys(letters)  # none automatically
+# # print(vowels)
+# # vowels = dict.fromkeys(letters, numbers)
+# # print(vowels)
+
+# # anything = ['a', 'b', 'c']
+# # anymany = [1, 2, 3]
+
+# # randomDict = dict.fromkeys(anything, anymany)
+# # print(randomDict)
+# # print(type(vowels))
+# # print(type(randomDict))
+
+# random = {}
+# print(type(random))
+
+players = {
+    "winger": "Kadir",
+    "striker": "Adil",
+    "defender": "Fozil",
+    "winger_number": 7,
+    "subs": "Bin, Maiwand, Gourab",
+    11: "squad amount",
+    "Best Player": "Kadir, 7, Uzbekistan",
+    "Active Players": True,
+    "Statistics": {"Win", "Win", "Lose", "Win"},
+    "Scores": ["11:1", "0:3", "5:4"],
+    "Cups": ("Sawo Gruz", "Nations Cup")
+}
+
+# # print({}.fromkeys(players))
+# print({}.get("random_key"))
+
+# print(players.items())
+# print(players.keys())
+# print(players.values())
+
+# for item1 in players.values():
+#     print(item1)
+clubs = {
+    1: 1,
+    2: 2,
+    3: 3,
+    4: 4,
+    5: 5
+}
+
+# we are merging two dicts
+players.update(clubs)
+print(players.items())
+
+players.update(dict(club="Real Madrid", opposite_team="Barcelona"))
+print(players)
+
+
+for i in players.items():
+    print(i)
+
+for i in players:
+    # print(i, players[i])
+    print(f"{i}: {players[i]}")
+
+for i in players.keys():
+    pass
